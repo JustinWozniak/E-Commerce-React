e2a34441ad21630f3a4def93a8c8f82bb5be48c6
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-04-08 14:37:43 -0700

    More work on our Google sign in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom'
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
-import Header from '../src/components/header/header.component'
-import SignInAndSignUpPage from '../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.coponent'
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "../src/components/header/header.component";
+import SignInAndSignUpPage from "../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.coponent";
+import { auth } from "../src/firebase/firebase.utils";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unSubscribeFromAuth = null;
 
 
-function App() {
-  return (
-    <div>
-    <Header />
-      <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-      <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+  //Next two methods open and close as users sigh in/out. Saving us from memory leaks in our app
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      //THIS IS WHERE WE GET USER INFO FOR OUR ENTIRE PROJECT
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
