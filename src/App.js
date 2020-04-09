e5c0786b370d77e670a842675ca23e05605f3d20
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-04-08 21:55:36 -0700

    Finished storing user info into firebase database apon sign in with Google account

--- src/App.js
+++ src/App.js
@@ -1,48 +1,58 @@
-import React from "react";
-import "./App.css";
-import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import Header from "../src/components/header/header.component";
-import SignInAndSignUpPage from "../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.coponent";
-import { auth } from "../src/firebase/firebase.utils";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from '../../ecommerce/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.coponent';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
-      currentUser: null,
-      userName:null
+      currentUser: null
     };
   }
 
-  unSubscribeFromAuth = null;
+  unsubscribeFromAuth = null;
 
-
-  //Next two methods open and close as users sigh in/out. Saving us from memory leaks in our app
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      // this.setState({ userName: user.displayName });
-      //THIS IS WHERE WE GET USER INFO FOR OUR ENTIRE PROJECT
-      // console.log(user.displayName);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
   componentWillUnmount() {
-    this.unSubscribeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render() {
-    // console.log(this.state.currentUser.)
     return (
       <div>
-        <Header currentUser={this.state.currentUser} displayName={this.state.displayName} />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
