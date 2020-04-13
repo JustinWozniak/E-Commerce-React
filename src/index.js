ba86bab7ee74d5d1b5c81cf6032dcbc59fa834ca
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-04-13 09:53:24 -0700

    building the REDUX 'store' for our project. Finished setup...

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import store from '../src/redux/store'
+
+
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
