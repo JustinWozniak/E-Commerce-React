b1f70c8a41825a7d147b48820f637fc364107854
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-04-13 13:19:05 -0700

    Starting Shopping Cart Component. Icon now shows in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import "./header.styles.scss"
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 
 const Header = ({ currentUser, displayName }) => (
@@ -22,6 +23,7 @@ const Header = ({ currentUser, displayName }) => (
                 currentUser ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :
                     <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 )
