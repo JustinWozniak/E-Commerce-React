fd15ebc3230e6030846dfdc45699bc28726ccc57
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-04-15 13:21:29 -0700

    Built a main directory so i could pull state into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,19 +1,21 @@
 import { combineReducers } from 'redux';
-import {persistReducer} from 'redux-persist'
+import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer'
 
-const persistConfig ={
-  key:'root',
+const persistConfig = {
+  key: 'root',
   storage,
-  whiteLis:['cart']
+  whiteLis: ['cart']
 }
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
-export default persistReducer(persistConfig,rootReducer)
+export default persistReducer(persistConfig, rootReducer)
