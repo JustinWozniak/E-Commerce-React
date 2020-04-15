a2ae1ddc43be01ec71931c1cb1e01897cb12efa3
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-04-15 11:48:46 -0700

    added session storage to project with Redux Persist...cart items stay with page is refreshed

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from '../src/redux/store'
-
+import { store, persiststor } from '../src/redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persiststor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
