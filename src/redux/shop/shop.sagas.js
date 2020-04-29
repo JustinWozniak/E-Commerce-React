bccb423cd790a5f5e280c1c1a51ecc57a6894b0e
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-04-29 00:42:03 -0700

    converted logic to use saga for async getting data from backend

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,20 @@
-import { takeEvery } from 'redux-saga/effects'
-
+import { takeEvery, call, put } from 'redux-saga/effects'
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
 import ShopActionTypes from './shop.types'
 
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions'
+
 export function* fetchCollectionsAsync() {
-   yield console.log("IVE BEEN FIRED")
+    yield console.log("IVE BEEN FIRED")
+    try {
+        const collectionRef = firestore.collection('collecions');
+        const snapshot = yield collectionRef.get()
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot)
+        yield put(fetchCollectionsSuccess(collectionsMap))
+    } catch (error) {
+yield put(fetchCollectionsFailure(error.message))
+    }
+   
 }
 export default function* fetchCollectionsStart() {
     yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START,
